6bcb5f625a85b918fc8f207f7d29b836ca4f44dc
SuiShiliang <1075670749@qq.com>
2018-10-16 17:02:34 +0800

    application 启动类添加mybatis 自动扫描【MapperScan】

--- src/main/java/com/ssl/springbootvue/SpringbootvueApplication.java
+++ src/main/java/com/ssl/springbootvue/SpringbootvueApplication.java
@@ -1,9 +1,11 @@
 package com.ssl.springbootvue;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ssl.springbootvue.dao")
 public class SpringbootvueApplication {
 
     public static void main(String[] args) {
